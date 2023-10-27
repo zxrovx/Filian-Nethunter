c2da4beb4c5581eb454c62dfa57fcc8f4d3d0534
ZXROVX <91070944+zxrovx@users.noreply.github.com>
2023-10-27 23:57:23 +0700

    Remove/out Directory

--- out/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.14.328 -Filian/e610c4f4cd"
--- out/include/generated/vdso32-offsets.h
+++
@@ -1,4 +0,0 @@
-#define vdso_offset_compat_rt_sigreturn_arm	0x0b80
-#define vdso_offset_compat_rt_sigreturn_thumb	0x0b94
-#define vdso_offset_compat_sigreturn_arm	0x0b74
-#define vdso_offset_compat_sigreturn_thumb	0x0b8c
